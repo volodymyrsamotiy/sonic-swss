53cf04ee65bfee395c876fca3459a6f066a961ce
Volodymyr Samotiy <volodymyrs@nvidia.com>
2021-06-25 18:26:30 +0300

    [PFCWD + Asym PFC]: Add VS test
    Signed-off-by: Volodymyr Samotiy <volodymyrs@nvidia.com>

--- tests/test_pfcwd.py
+++ tests/test_pfcwd.py
@@ -12,6 +12,71 @@ PFCWD_TC = ["3", "4"]
 PFCWD_RULE_NAME_1 =  "DROP_TEST_RULE_1"
 PFCWD_RULE_NAME_2 =  "DROP_TEST_RULE_2"
 
+
+# Define fake platform for "DVS" fixture, it will set "platform" environment variable for the "orchagent" code.
+# It is needed for the "test_PfcWdAsym" test case because some PFCWD "orchagent" code is under "platform" condition.
+# There is no implementation for the virtual switch so as a result PFCWD cannot correctly initialize.
+DVS_FAKE_PLATFORM = "mellanox"
+
+
+def setPortPfc(dvs, port_name, pfc_queues):
+    cfg_db = swsscommon.DBConnector(swsscommon.CONFIG_DB, dvs.redis_sock, 0)
+    port_qos_tbl = swsscommon.Table(cfg_db, 'PORT_QOS_MAP')
+
+    fvs = swsscommon.FieldValuePairs([('pfc_enable', ",".join(str(q) for q in pfc_queues))])
+    port_qos_tbl.set(port_name, fvs)
+
+
+def getPortOid(dvs, port_name):
+    cnt_db = swsscommon.DBConnector(swsscommon.COUNTERS_DB, dvs.redis_sock, 0)
+    port_map_tbl = swsscommon.Table(cnt_db, 'COUNTERS_PORT_NAME_MAP')
+
+    for k in port_map_tbl.get('')[1]:
+        if k[0] == port_name:
+            return k[1]
+
+    return ''
+
+
+def setPortPfcAsym(dvs, port_name, pfc_asym):
+    cfg_db = swsscommon.DBConnector(swsscommon.CONFIG_DB, dvs.redis_sock, 0)
+
+    port_tbl = swsscommon.Table(cfg_db, 'PORT')
+    fvs = swsscommon.FieldValuePairs([('pfc_asym', pfc_asym)])
+    port_tbl.set(port_name, fvs)
+
+
+def startPfcWd(dvs, port_name):
+    dvs.runcmd("pfcwd start --action drop --restoration-time 400 {} 400".format(port_name))
+
+
+def verifyPfcWdCountersList(dvs, port_oid):
+
+    pfc_wd_db = swsscommon.DBConnector(swsscommon.PFC_WD_DB, dvs.redis_sock, 0)
+    flex_cnt_tbl = swsscommon.Table(pfc_wd_db, 'FLEX_COUNTER_TABLE')
+
+    expected_counters_list = ['SAI_PORT_STAT_PFC_0_RX_PAUSE_DURATION_US',
+                              'SAI_PORT_STAT_PFC_1_RX_PAUSE_DURATION_US',
+                              'SAI_PORT_STAT_PFC_2_RX_PAUSE_DURATION_US',
+                              'SAI_PORT_STAT_PFC_3_RX_PAUSE_DURATION_US',
+                              'SAI_PORT_STAT_PFC_4_RX_PAUSE_DURATION_US',
+                              'SAI_PORT_STAT_PFC_5_RX_PAUSE_DURATION_US',
+                              'SAI_PORT_STAT_PFC_6_RX_PAUSE_DURATION_US',
+                              'SAI_PORT_STAT_PFC_7_RX_PAUSE_DURATION_US',
+                              'SAI_PORT_STAT_PFC_0_RX_PKTS',
+                              'SAI_PORT_STAT_PFC_1_RX_PKTS',
+                              'SAI_PORT_STAT_PFC_2_RX_PKTS',
+                              'SAI_PORT_STAT_PFC_3_RX_PKTS',
+                              'SAI_PORT_STAT_PFC_4_RX_PKTS',
+                              'SAI_PORT_STAT_PFC_5_RX_PKTS',
+                              'SAI_PORT_STAT_PFC_6_RX_PKTS',
+                              'SAI_PORT_STAT_PFC_7_RX_PKTS']
+
+    counters_list = flex_cnt_tbl.get('PFC_WD:{}'.format(port_oid))[1][0][1].split(',')
+
+    assert sorted(counters_list) == sorted(expected_counters_list), "PFCWD is not started for all PFC priorities"
+
+
 class TestPfcWd:
     def test_PfcWdAclCreationDeletion(self, dvs, dvs_acl, testlog):
         try:
@@ -77,6 +142,31 @@ class TestPfcWd:
 
         finally:
             dvs_acl.remove_acl_table(PFCWD_TABLE_NAME)
+
+    '''
+    Verifies that PFC WD starts for all priorities in case of Asymmetric PFC is enabled
+    '''
+    def test_PfcWdAsym(self, dvs, testlog):
+
+        port_name = 'Ethernet0'
+        pfc_queues = [ 3, 4 ]
+
+        # Configure default PFC
+        setPortPfc(dvs, port_name, pfc_queues)
+
+        # Get SAI object ID for the interface
+        port_oid = getPortOid(dvs, port_name)
+
+        # Enable asymmetric PFC
+        setPortPfcAsym(dvs, port_name, 'on')
+
+        # Start PFCWD
+        startPfcWd(dvs, port_name)
+
+        # Verify that PFC WD was started for all PFC priorities
+        verifyPfcWdCountersList(dvs, port_oid)
+
+
 #
 # Add Dummy always-pass test at end as workaroud
 # for issue when Flaky fail on final test it invokes module tear-down before retrying
